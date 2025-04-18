0d29c345da4af6b5102a0b7b5302e8cd1fc5fb5c
AmarNathPrajapati <92644905+AmarNathPrajapati@users.noreply.github.com>
2025-04-18 17:41:15 +0530

    save changes

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -1,7 +1,7 @@
 export const Navbar = () =>{
     return (
         <>
-            Navbar Page
+            Navbar sdfsdfsd Page
         </>
     )
 }
--- client/src/components/Sidebar/index.tsx
+++ client/src/components/Sidebar/index.tsx
@@ -1,7 +1,7 @@
 export const Sidebar = () =>{
     return (
         <>
-            Sidebar pages
+            Sidebar sfsdfsdf pages
         </>
     )
 }
